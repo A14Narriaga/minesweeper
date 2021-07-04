bf0a4b0581ecf32f781bcedda41de8b5759422cb
Alan Arriaga <A14N.arriaga@gmail.com>
2021-07-03 22:34:06 -0500

    Update Board.tsx

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,6 +7,13 @@ const generateBoard = ({ rows, cols }: { rows: number; cols: number }) => {
   const board = Array.from(Array(rows)).map(() =>
     Array.from(Array(cols)).map(_ => ({ cover: true, type: "" } as cell))
   ) as Array<Array<cell>>
+  const randomCell = (): { row: number; col: number } => {
+    return { row: Math.floor(Math.random() * rows), col: Math.floor(Math.random() * cols) }
+  }
+  for (let i = 0; i < 10; i++) {
+    const { row, col } = randomCell()
+    board[row][col].type = "mine"
+  }
   return board
 }
 
@@ -17,23 +24,23 @@ const reducer = (board: Array<Array<cell>>, accion: setCover) => {
   if (type === "setCover") {
     const newBoard = [...board]
     newBoard[i][j].cover = false
-    newBoard[i][j].type = "mine"
-    console.log(i, j)
     return newBoard
   }
   return board
 }
 
 const Board = ({ mine }: { mine: string }) => {
-  const [board, dispatchBoard] = useReducer(reducer, [], () => generateBoard({ rows: 10, cols: 10 }))
+  const [board, dispatchBoard] = useReducer(reducer, [], () =>
+    generateBoard({ rows: 10, cols: 10 })
+  )
 
   return (
     <section
       className="board"
       style={{
         gridTemplateColumns: `repeat(${board.length}, 1fr)`,
-        width: `${2*board.length+(board.length-1)*0.05+1}rem`,
-        height: `${2*board[0].length+(board[0].length-1)*0.05+1}rem`,
+        width: `${2 * board.length + (board.length - 1) * 0.05 + 1}rem`,
+        height: `${2 * board[0].length + (board[0].length - 1) * 0.05 + 1}rem`,
       }}
     >
       {board.map((row, i) =>
@@ -43,7 +50,7 @@ const Board = ({ mine }: { mine: string }) => {
             onClick={() => dispatchBoard({ type: "setCover", i, j })}
             className={`box box-${cover ? "" : "uncover"}`}
           >
-            <i className={type === "mine" ? `fa fa-${mine}` : ""}></i>
+            {!cover ? <i className={type === "mine" ? `fa fa-${mine}` : ""}></i> : <i></i>}
           </button>
         ))
       )}
