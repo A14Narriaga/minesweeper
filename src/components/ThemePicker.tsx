dae3da3833324d52abb535df94bbc20efa869033
Alan Arriaga <A14N.arriaga@gmail.com>
2021-07-15 00:07:00 -0500

    Modal background update

---
+++ src/components/ThemePicker.tsx
@@ -0,0 +1,14 @@
+const ThemePicker = () => {
+  return (
+    <section>
+      <div className={`theme-picker`}>
+        <div className="picker">
+          <section className="theme"></section>
+          <section className="color"></section>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ThemePicker
